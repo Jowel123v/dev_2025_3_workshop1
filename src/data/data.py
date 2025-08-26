f69581674717f172f0536eb39bd6c0a2922705b1
Jowel123v <jdmarin12@ucatolica.edu.co>
2025-08-26 14:53:54 -0500

    buscar e invertir lista

--- src/data/data.py
+++ src/data/data.py
@@ -14,8 +14,9 @@ class Data:
         Returns:
             list: Lista con los elementos en orden inverso
         """
-        pass
-    
+        lista_invertida = list(reversed(lista))
+        return lista_invertida        
+
     def buscar_elemento(self, lista, elemento):
         """
         Busca un elemento en una lista y devuelve su índice (o -1 si no existe).
@@ -28,7 +29,11 @@ class Data:
         Returns:
             int: Índice del elemento o -1 si no se encuentra
         """
-        pass
+        for i in range(len(lista)):
+          if lista[i] == elemento:
+            return i
+        return -1
+  
     
     def eliminar_duplicados(self, lista):
         """
